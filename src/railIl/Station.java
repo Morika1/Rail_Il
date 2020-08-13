66c5913909234bfdd3f21ac768c8a0f5e6ed7d33
Morika1 <68543807+Morika1@users.noreply.github.com>
2020-08-13 17:37:56 +0300

    added a new station class

---
+++ src/railIl/Station.java
@@ -0,0 +1,19 @@
+package railIl;
+
+public class Station {
+
+	private String stationName;
+
+	public Station(String stationName) {
+		setStationName(stationName);
+	}
+
+	public String getStationName() {
+		return stationName;
+	}
+
+	public void setStationName(String stationName) {
+		this.stationName = stationName;
+	}
+
+}

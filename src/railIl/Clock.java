c426c50e8dd833f0bad72abbb0cb035ee75803f2
Morika1 <68543807+Morika1@users.noreply.github.com>
2020-09-08 22:41:33 +0300

    Update clock

--- src/railIl/Clock.java
+++ src/railIl/Clock.java
@@ -1,6 +1,6 @@
 package railIl;
 
-public class Clock {
+public class Clock  {
 
 	private int minutes;
 	private int hours;
@@ -10,16 +10,21 @@ public class Clock {
 		setMinutes(minutes);
 	}
 	
+	public Clock() throws Exception {
+		this(0,0);
+	}
+
 	public int getMinutes() {
 		return minutes;
 	}
 
 	public void setMinutes(int minutes) throws Exception {
 		if (minutes < 0 || minutes > 59)
-			throw new Exception("Minutes can be between 0 to 59");
+			 throw new Exception("Minutes can be between 0 to 59");
 
 		else
-			this.minutes = minutes;
+		this.minutes = minutes;
+		
 	}
 
 	public int getHours() {
@@ -27,23 +32,27 @@ public class Clock {
 	}
 
 	public void setHours(int hours) throws Exception {
-		if (hours < 1 || hours > 24)
-			throw new Exception("Hours can be between 1 to 24");
-
+		if (hours < 1 || hours > 23)
+			throw new Exception("Hours can be between 1 to 23");
+			
 		else
-			this.hours = hours;
+		this.hours = hours;
+		
 	}
 
-	public void show() {
+	public String toSring() {
+		String time = new String();
 		if (hours < 10)
-			System.out.print("0");
-
-		System.out.print(hours + ":");
-
+			time += "0" + hours + ":";
+		else
+			time += hours + ":";
 		if (minutes < 10)
-			System.out.print("0");
-
-		System.out.print("minutes");
+			time += "0" + minutes;
+		else
+			time += minutes;
+		return time;
 	}
+
 
 }
+

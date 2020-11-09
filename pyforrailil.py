9d00caaf761b40b1656857e361227155538c8637
Morika1 <68543807+Morika1@users.noreply.github.com>
2020-11-09 22:34:51 +0200

    configure bin

--- pyforrailil.py
+++ pyforrailil.py
@@ -12,5 +12,8 @@ def rail():
                                     "bin", "user.UsersMain",
                                     request.args.get('outformat'),request.args.get('source'),
                                     request.args.get('destination'),request.args.get('hour'),request.args.get('minutes')])
-
+if __name__=="__main__";
+   subprocess.run(["javac", "-d", "bin", "src/railil/*.java"])
+   subprocess.run(["javac", "-d", "bin", "src/user/*.java"])
+   
 app.run(port=8000, host="0.0.0.0")

cf1d10cc5a36108fab642e0eabdefe6a69fe9618
Morika1 <68543807+Morika1@users.noreply.github.com>
2020-11-11 12:54:09 +0200

    adaptToCompilerOnUbuntu

--- src/tests/TrainStationTest.java
+++ src/tests/TrainStationTest.java
@@ -15,11 +15,11 @@ public class TrainStationTest {
 		TrainStation trainStation = createTrainStation();
 		StringBuffer expectedResult = new StringBuffer();
 		expectedResult.append("1 Travel details: ");
-		expectedResult.append("Tel Aviv, 06:00, Raanana, 06:40, Haifa, 07:30, Rosh Pina, 09:00, \n");
+		expectedResult.append("Tel Aviv, 06:00, Raanana, 06:40, Haifa, 07:30, Rosh Pina, 09:00\n");
 		expectedResult.append("2 Travel details: ");
-		expectedResult.append("Petah Tikva, 06:30, Herzelia, 07:30, Tel Aviv, 08:30, \n");
+		expectedResult.append("Petah Tikva, 06:30, Herzelia, 07:30, Tel Aviv, 08:30\n");
 		expectedResult.append("3 Travel details: ");
-		expectedResult.append("Petah Tikva, 07:00, Tel Aviv, 08:00, Herzelia, 08:30, Haifa, 09:40, \n");
+		expectedResult.append("Petah Tikva, 07:00, Tel Aviv, 08:00, Herzelia, 08:30, Haifa, 09:40\n");
 
 		assertEquals(expectedResult.toString(), trainStation.showTravelList());
 	}
